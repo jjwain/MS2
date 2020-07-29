f07997ddffa5aa478009fa085ca0c45f7217b03f
Jordan Wain <jordanwainy@gmail.com>
2020-07-29 20:47:38 +0100

    updated specific maps

--- assets/scripts/edinburgh-map.js
+++ assets/scripts/edinburgh-map.js
@@ -1,30 +1,33 @@
 function initMap(){
     var startPoint = {
-    zoom: 13,
-    center: {lat:51.5113,lng:-0.1137},
-    minZoom: 11
+    zoom: 12,
+    center: {lat:55.9533,lng:-3.1883}
     }
     // Add the map to the website
-var map = new
-google.maps.Map(document.getElementById('map'), startPoint);
+    var map = new
+    google.maps.Map(document.getElementById('map'), startPoint);
 
 var markers = [
     
-        {coords:{lat: 51.5138,lng:-0.0984},
-        content:'St. Pauls Cathedral'},
+        {coords:{lat: 55.9486,lng: -3.1999},
+        content:'Edinburgh Castle'},
 
-        {coords:{lat: 51.5081,lng:-0.0759},
-        content:'Tower of London'},
+        {coords:{lat: 55.9490,lng: -3.1899},
+        content:'Edinburgh Old Town'},
 
-        {coords:{lat: 51.5194,lng:-0.1270},
-        content:'The British Museum'},
+        {coords:{lat: 55.9495,lng: -3.1909},
+        content:"St Giles' Cathedral"},
         
-        {coords:{lat: 51.5033,lng:-0.1195},
-        content:'The London Eye'},
+        {coords:{lat: 55.9441,lng: -3.1618},
+        content:"Arthur's Seat"},
         
-        {coords:{lat:51.5014,lng:-0.1419},
-        content:'Buckingham Palace'}              
+        {coords:{lat: 55.9524,lng: -3.1933},
+        content:'Scott Monument'},                
         ];
+    
+        for(var i = 0; i < markers.length; i++){
+            addMarker(markers[i]);
+        }
     
         for(var i = 0; i < markers.length; i++){
           addMarker(markers[i]);
@@ -53,7 +56,3 @@ var markers = [
     };
 }
 }
-
-$("#lon-1").click(function(){
-  $(".dropDown1").fade("1000").add("#lon1-info");
-}); 
---
+++ assets/scripts/saint-malo-map.js
@@ -0,0 +1,55 @@
+function initMap(){
+    var startPoint = {
+    zoom: 14,
+    center: {lat:48.6493,lng:-2.0257}
+    }
+    // Add the map to the website
+var map = new
+google.maps.Map(document.getElementById('map'), startPoint);
+
+var markers = [
+    
+        {coords:{lat: 48.6495,lng:-2.0257},
+        location:'Saint-Malo Cathedral'},
+
+        {coords:{lat: 48.6550,lng:-2.0232 },
+        content:'Fort National'},
+
+        {coords:{lat: 48.6195,lng:-1.9945},
+        content:'Grand Aquarium Saint Malo'},
+        
+        {coords:{lat: 48.6522,lng:-2.0331},
+        content:'Grand Bé'},
+        
+        {coords:{lat: 48.6386,lng:-2.0186},
+        content:'Petit Bé'},
+];
+    
+        for(var i = 0; i < markers.length; i++){
+            addMarker(markers[i]);
+        }
+        
+
+        function addMarker(props){
+            var marker = new google.maps.Marker({
+              position:props.coords,
+              map:map,
+            });
+      
+              // Opens window displaying Location Information
+              if(props.content){
+                var infoWindow = new google.maps.InfoWindow({
+                  content:props.content
+                });
+      
+                marker.addListener('click', function(){
+                  infoWindow.open(map, marker);
+                });
+
+            // if(props.content){
+            //     var infoWindow = new google.maps.InfoWindow({
+            //       content:props.content
+            //     });
+    };
+}
+}
